873be119dab0d96d519861bb5818f3acbc7d8203
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-07 18:58:21 +0100

    challenge 1 and 2 done

--- src/server.js
+++ src/server.js
@@ -2,4 +2,26 @@ const express = require("express");
 
 const server = express();
 
+// Create a new route for the homepage at GET /.
+// It should return an HTML body including a <h1>Hello Express</h1>.
+server.get("/", (request, response) => {
+  response.send("<h1>Hello Express</h1>");
+});
+
+
+//Create a new route at GET /colour.
+//It should check the URL's search parameters for a hex property.
+//If present the returned HTML page should have its <body> element's background-color set to the hex value.
+//E.g. this request GET /colour?hex=ff0000 should result in an HTML page with a red background.
+//If hex is not present the background should be white.
+
+server.get("/colour", (request, response) => {
+  const hex = request.query.hex;
+  if (hex) {
+    response.send(`<body style="background-color: #${hex}">`);
+  } else {
+    response.send(`<body style="background-color: #ffffff">`);
+  }
+});
+
 module.exports = server;
