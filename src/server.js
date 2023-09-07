7f9d36b5a599350e49c1123a47c848c2c0031279
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-07 19:25:16 +0100

    challenge 4 and 5 done

--- src/server.js
+++ src/server.js
@@ -48,5 +48,44 @@ server.get("/colour", (req, res) => {
   `;
   res.send(html);
 });
+/*
+Create a new route GET /cheese.
+It should return an HTML response containing a form for submitting new cheeses.
+The form should send POST requests to the same page.
+It should include a text input for the cheese name and a range input for the cheese's rating (from 0 to 5).
+*/
+const cheeseRatings = [];
+server.get("/cheese", (req, res) => {
+  const cheeseList = cheeseRatings.map((cheese) => `<li>${cheese.name} - ${cheese.rating}</li>`);
+
+  const html = `
+    <form method="POST">
+      <label for="name">Cheese name</label>
+      <input type="text" name="name">
+      <label for="rating">Rating</label>
+      <input type="range" name="rating" min="0" max="5">
+      <button type="submit">Add cheese</button>
+    </form>
+    <ul>
+      ${cheeseList.join("")}
+    </ul>
+  `
+  res.send(html);
+})
+
+/*
+Create a new route POST /cheese. It should receive the POST request from the previous form and use the built-in Express middleware to read the url-encoded request body.
+
+It should store each cheese rating in an array outside of the handler function, so other routes can access this information. Once the new rating is pushed to this array it should redirect back to the same page.
+
+Submitting the form should result in the page reloading and displaying the newly added cheese in the list.
+*/
+
+server.post("/cheese", express.urlencoded({ extended: true }), (req, res) => {
+  const { name, rating } = req.body;
+  cheeseRatings.push({ name, rating });
+  res.redirect("/cheese");
+});
+
 
 module.exports = server;
