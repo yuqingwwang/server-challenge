3e44e390356a790e3002d780ab6b3262bc3873cd
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-07 19:17:46 +0100

    challenge 3 done

--- src/server.js
+++ src/server.js
@@ -14,6 +14,25 @@ server.get("/", (request, response) => {
 // If present the returned HTML page should have its <body> element's background-color set to the hex value.
 // If hex is not present the background should be white.
 
+// server.get("/colour", (req, res) => {
+//   const hex = req.query.hex || "ffffff";
+//   const html = `
+//     <style>
+//       body {
+//         background-color: #${hex};
+//       }
+//     </style>
+//   `;
+//   res.send(html);
+// });
+
+/*
+Edit your GET /colour route to include a form in the HTML response.
+This form should include an input for the user to type in a hex code.
+When the form is submitted a GET request will be sent to the same route,
+which will change the background of the page to whatever the user entered.
+*/
+
 server.get("/colour", (req, res) => {
   const hex = req.query.hex || "ffffff";
   const html = `
@@ -22,6 +41,10 @@ server.get("/colour", (req, res) => {
         background-color: #${hex};
       }
     </style>
+    <form>
+      <label for="hex">Enter a hex colour code</label>
+      <input type="text" name="hex" value="${hex}">
+    </form>
   `;
   res.send(html);
 });
